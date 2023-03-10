7b914172b0d902ce37c9b816b1d17c7a83eb4769
Pur3-MAlice <aliceford98@live.co.uk>
2023-03-10 21:10:24 +0000

    add function prompts and ensure worksheet data pulls through

--- run.py
+++ run.py
@@ -13,3 +13,22 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('reading_list')
 TBR = SHEET.worksheet('tbr')
 READ = SHEET.worksheet('read')
+
+tbr_data = TBR.get_all_values()
+print(tbr_data)
+
+read_data = READ.get_all_values()
+print(read_data)
+
+# USER TO CHOOSE ADD TO LIST, LOOK AT LIST?
+# AFTER THIS THEN WHICH LIST
+
+# USER TO ADD DATA TO READ
+# TITLE, ATHOUR, DATE, NUMBER
+
+# USER TO ADD DATA TO TBR
+# TITLE, ATHOUR, DATE, NUMBER
+
+
+# MAIN FUNCTION HERE THAT IS DEPLOYED AT END
+# main()
