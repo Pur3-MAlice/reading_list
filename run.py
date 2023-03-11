f84f2ef6ddf644f097d551fdd765ff31d4d40bf4
Pur3-MAlice <aliceford98@live.co.uk>
2023-03-11 15:24:14 +0000

    docs:adding in bones of functions

--- run.py
+++ run.py
@@ -13,6 +13,10 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('reading_list')
 TBR = SHEET.worksheet('tbr')
 READ = SHEET.worksheet('read')
+MENU_PROMT = '''Welcome back to your reading list! 
+Press "T" to go to your TBR. Press "R" to go to your Read list.
+Or press "B" to go to the About Section
+'''
 
 tbr_data = TBR.get_all_values()
 # print(tbr_data)
@@ -20,34 +24,52 @@ tbr_data = TBR.get_all_values()
 read_data = READ.get_all_values()
 # print(read_data)
 
-MENU_PROMT = '''Welcome back to your reading list! 
-Press "T" to go to your TBR. Press "R" to go to your Read list.
-Or press "B" to go to the About Section
-'''
+
+def tbr_choice():
+    '''
+    to choose wether to add in new boook or to print out tbr list
+    '''
+    while selected_option != "":
+        print()
+
+
+def read_choice():
+    '''
+    docstring
+    '''
+    # print()
+
+
+def add_book():
+    '''
+    docstring
+    '''
+    # print(f"Adding {book_title}\n")
+
+
+def list_book():
+    '''
+    docstring
+    '''
+    # print(f"displaying {list_name}\n")
+
 
 selected_option = input(MENU_PROMT).strip().lower()
 
-while selected_option != "b":
+while selected_option != "":
     if selected_option == "t":
         print("Going to TBR list...\n")
-        break
+        tbr_choice()
     elif selected_option == "r":
         print("Going to READ list..\n")
+        break
+    elif selected_option == "b":
+        print("Going to About Section...\n")
         break
     else:
         print(f"'{selected_option}' is not valid option. Please try again")
 
-    selected_option = input(MENU_PROMT).strip().lower()    
-
-
-# USER TO CHOOSE ADD TO LIST, LOOK AT LIST?
-# AFTER THIS THEN WHICH LIST
-
-# USER TO ADD DATA TO READ
-# TITLE, ATHOUR, DATE, NUMBER
-
-# USER TO ADD DATA TO TBR
-# TITLE, ATHOUR, DATE, NUMBER
+    selected_option = input(MENU_PROMT).strip().lower()
 
 
 # MAIN FUNCTION HERE THAT IS DEPLOYED AT END
