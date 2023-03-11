61ff65fa0f55fbe28af183adaca81f5ec3f62d9a
Pur3-MAlice <aliceford98@live.co.uk>
2023-03-11 13:24:08 +0000

    feat: add MENU_PROMPT. User input navigation

--- run.py
+++ run.py
@@ -15,10 +15,30 @@ TBR = SHEET.worksheet('tbr')
 READ = SHEET.worksheet('read')
 
 tbr_data = TBR.get_all_values()
-print(tbr_data)
+# print(tbr_data)
 
 read_data = READ.get_all_values()
-print(read_data)
+# print(read_data)
+
+MENU_PROMT = '''Welcome back to your reading list! 
+Press "T" to go to your TBR. Press "R" to go to your Read list.
+Or press "B" to go to the About Section
+'''
+
+selected_option = input(MENU_PROMT).strip().lower()
+
+while selected_option != "b":
+    if selected_option == "t":
+        print("Going to TBR list...\n")
+        break
+    elif selected_option == "r":
+        print("Going to READ list..\n")
+        break
+    else:
+        print(f"'{selected_option}' is not valid option. Please try again")
+
+    selected_option = input(MENU_PROMT).strip().lower()    
+
 
 # USER TO CHOOSE ADD TO LIST, LOOK AT LIST?
 # AFTER THIS THEN WHICH LIST
